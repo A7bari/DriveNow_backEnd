d2dbbe3d21fc209e0892fa76ca33ad326ac37276
Ahbari-M <mohammed.ahbari@usmba.ac.ma>
2022-12-29 14:18:38 +0100

    add User Login and register DTOs

---
+++ DriveNow/Dtos/UserLoginDto.cs
@@ -0,0 +1,9 @@
+﻿namespace DriveNow.Dtos
+{
+    public class UserLoginDto
+    {
+        public string Email { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty; 
+    }
+}
---
+++ DriveNow/Dtos/UserRegisterDto.cs
@@ -0,0 +1,15 @@
+﻿namespace DriveNow.Dtos
+{
+    public class UserRegisterDto
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+
+        public string RetipedPassword { get; set; } = string.Empty;
+
+    }
+}
