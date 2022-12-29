c993a3abb7375f937c9f1ce120c9897ea303a081
Ahbari-M <mohammed.ahbari@usmba.ac.ma>
2022-12-29 14:17:57 +0100

    add User model

---
+++ DriveNow/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace DriveNow.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Email { get; set; }= string.Empty;
+
+        public byte[] PasswordHash { get; set; } 
+
+        public byte[] PasswordSalt { get; set; }
+    }
+}
