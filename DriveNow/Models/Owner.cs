8b03fd1c1cf25c3eb5589433457a8ff5ebea1c33
Ahbari-M <mohammed.ahbari@usmba.ac.ma>
2022-12-30 19:52:26 +0100

    create Owner and Tenant models

---
+++ DriveNow/Models/Owner.cs
@@ -0,0 +1,21 @@
+﻿using MessagePack;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DriveNow.Models
+{
+    public class Owner
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int OwnerId { get; set; }
+
+        [Required]
+        public string CIN { get; set; }
+
+        [Required]
+        public string Adress { get; set; }
+
+        [Required]
+        public bool HasAgancy { get; set; } = false;
+    }
+}
---
+++ DriveNow/Models/Tenant.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DriveNow.Models
+{
+    public class Tenant
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TenantId { get; set; }
+
+        [Required]
+        public string CIN { get; set; }
+    }
+}
