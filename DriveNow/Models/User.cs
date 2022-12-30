bfeb2e8b15ca62c66c2890decec25176aafbbcc7
Ahbari-M <mohammed.ahbari@usmba.ac.ma>
2022-12-30 17:47:51 +0100

    update user model

--- DriveNow/Models/User.cs
+++ DriveNow/Models/User.cs
@@ -1,13 +1,20 @@
-﻿namespace DriveNow.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DriveNow.Models
 {
     public class User
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
+        [Required]
         public string FirstName { get; set; } = string.Empty;
 
+        [Required]
         public string LastName { get; set; } = string.Empty;
 
+        [Required, EmailAddress]
         public string Email { get; set; }= string.Empty;
 
         public byte[] PasswordHash { get; set; } 
