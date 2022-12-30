f0b7625fc308e2cd06ac56697438bdf6a45fe129
NohailaElmasaoul <elmasaoulnohaila@gmail.com>
2022-12-30 18:45:47 +0000

    Contrat class commit

---
+++ DriveNow/Models/Contrat.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DriveNow.Models
+{
+    public class Contrat
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public float Amount { get; set; }
+        [Required]
+        public ReservationPeriods? reservationPeriods { get; set; }
+    }
+}
