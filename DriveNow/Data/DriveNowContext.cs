bdc0bcc991a53ae9154adc5516c4183908d3cb03
Ahbari-M <mohammed.ahbari@usmba.ac.ma>
2022-12-30 17:47:33 +0100

    create dbContext (code first)

---
+++ DriveNow/Data/DriveNowContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DriveNow.Models;
+
+namespace DriveNow.Data
+{
+    public class DriveNowContext : DbContext
+    {
+        public DriveNowContext (DbContextOptions<DriveNowContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DriveNow.Models.User> User { get; set; } = default!;
+    }
+}
