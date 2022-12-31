69c4782171e94fa4f21bf34fc8d8861f8fb3349e
AmmariAbdelmounaim <abdelmounaimammari@gmail.com>
2022-12-31 14:27:31 +0100

    update cardto

---
+++ DriveNow/Dtos/CarDto.cs
@@ -0,0 +1,30 @@
+﻿using DriveNow.Models;
+using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DriveNow.Dtos
+{
+    public class CarDto
+    {
+
+        [Required]
+        public float Price { get; set; }
+
+        [Required]
+        public string Brand { get; set; } = string.Empty;
+
+        [Required]
+        public string Color { get; set; } = string.Empty;
+        [Required]
+        public int ProductionYear { get; set; }
+        [Required]
+
+        public FType FuelType { get; set; } = FType.Gasoline;
+        [Required]
+        public int Km { get; set; }
+        [Required]
+        public float LocationPrice { get; set; }
+        [Required]
+        public string Description { get; set; } = string.Empty;
+    }
+}
