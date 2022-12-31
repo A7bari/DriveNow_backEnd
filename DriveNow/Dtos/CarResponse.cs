cf26564b0709de3a7227a3e767dd748688dfe8a0
AmmariAbdelmounaim <abdelmounaimammari@gmail.com>
2022-12-31 14:27:57 +0100

    create carresponse for pagination and count

---
+++ DriveNow/Dtos/CarResponse.cs
@@ -0,0 +1,12 @@
+﻿using DriveNow.Models;
+
+namespace DriveNow.Dtos
+{
+    public class CarResponse
+    {
+        public List<Car> Cars { get; set; } = new List<Car>();
+        public int Pages { get; set; }
+        public int CurrentPage { get; set; }
+        public int CarsCount { get; set; }  
+    }
+}
