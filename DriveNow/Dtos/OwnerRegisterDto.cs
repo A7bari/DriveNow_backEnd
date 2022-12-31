e3a6e6060bbd094e39c72bb89942e49ae3663b59
Ahbari-M <mohammed.ahbari@usmba.ac.ma>
2022-12-31 12:22:18 +0100

    create register Dtos for all users

---
+++ DriveNow/Dtos/OwnerRegisterDto.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DriveNow.Dtos
+{
+    public class OwnerRegisterDto
+    {
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+
+        [Compare("Password")]
+        public string RetipedPassword { get; set; } = string.Empty;
+
+        [Required]
+        public string CIN { get; set; }
+
+        [Required]
+        public string Adress { get; set; }
+
+        [Required]
+        public bool HasAgancy { get; set; } = false;
+
+    }
+}
