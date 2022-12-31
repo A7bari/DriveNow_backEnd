30473baf48d6529e936bae3255bfe96c5e3781e6
Ahbari-M <mohammed.ahbari@usmba.ac.ma>
2022-12-31 12:20:22 +0100

    make user abstract and add role feiled

--- DriveNow/Models/User.cs
+++ DriveNow/Models/User.cs
@@ -1,9 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using DriveNow.Helpers;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DriveNow.Models
 {
-    public class User
+    public abstract class User
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
@@ -20,5 +21,9 @@ namespace DriveNow.Models
         public byte[] PasswordHash { get; set; } 
 
         public byte[] PasswordSalt { get; set; }
+
+        [Required]
+        public Roles Role { get; set; }
+
     }
 }
