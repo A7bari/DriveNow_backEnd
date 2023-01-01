24f1defe24f59489f6b16ee0edf56ba0d87db8f5
AmmariAbdelmounaim <abdelmounaimammari@gmail.com>
2023-01-01 21:58:59 +0100

    add reservationPeriode dto

---
+++ DriveNow/Dtos/ReservationPeriodDto.cs
@@ -0,0 +1,13 @@
+﻿using DriveNow.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DriveNow.Dtos
+{
+    public class ReservationPeriodDto
+    {
+     
+        public DateTime StartDate { get; set; } = DateTime.Now;
+        public DateTime EndDate { get; set; }
+
+    }
+}
