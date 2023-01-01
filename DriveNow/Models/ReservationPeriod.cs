67c5b8f08da7a24dac31d67408ea17d625cdf54c
AmmariAbdelmounaim <abdelmounaimammari@gmail.com>
2023-01-01 15:57:55 +0100

    remove the s

---
+++ DriveNow/Models/ReservationPeriod.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DriveNow.Models
+{
+    public class ReservationPeriod
+    {
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public DateTime StartDate { get; set; } = DateTime.Now;
+
+        [Required]
+        public DateTime EndDate { get; set; }
+        public List<Car> Cars { get; set; }
+
+    }
+}
