82a8f4679218fc5dacc828094af27af67c5717f0
Ammari Abdelmounaim <102379343+AmmariAbdelmounaim@users.noreply.github.com>
2023-01-02 17:18:27 +0100

    Update Filters.cs

--- DriveNow/Helpers/Filters.cs
+++ DriveNow/Helpers/Filters.cs
@@ -4,7 +4,7 @@ namespace DriveNow.Helpers
 {
     public class Filters
     {
-        public string? key = null;
+        public string? key{get;set;} = null;
         public int? minPrice { get; set; } = null;
         public int? maxPrice { get; set; } = null;
         public int? maxkilometrage { get; set; } = null;
